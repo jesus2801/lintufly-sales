ec4606c33be4f980271762606cfeddb5b551155c
jesus2801 <jesusdavid280147@gmail.com>
2021-06-08 10:25:02 -0500

    rollbar agregado y manejo de errores mejorado

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -2,7 +2,10 @@
 export default {
   server: {
     clientOrigin: process.env.CLIENT_ORIGIN!,
-    secret: process.env.SECRET_KEY!
+    secret: process.env.SECRET_KEY!,
+    rollbar: {
+      accessToken: process.env.ROLLBAR_ACCESS_TOKEN!,
+    },
   },
   db: {
     uri: process.env.MONGO_URI!,
